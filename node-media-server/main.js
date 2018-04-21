d71c46dd796d9a948e78a1800e7ca93629316db0
Yichuan Chen <14307110157@fudan.edu.cn>
2018-04-21 11:52:32 +0800

    add rtmp server

---
+++ node-media-server/main.js
@@ -0,0 +1,14 @@
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 60,
+    ping_timeout: 30
+  },
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
